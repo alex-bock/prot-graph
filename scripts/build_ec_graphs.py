386de799751f4cb58c709b31b5d2ddb97e16284e
Alexander Bock <alex.bock@rice.edu>
2024-03-12 16:01:58 -0500

    parallelize graph building

--- scripts/build_ec_graphs.py
+++ scripts/build_ec_graphs.py
@@ -1,7 +1,8 @@
 
+import functools
 import os
 import sys
-import tqdm
+from tqdm.contrib.concurrent import process_map
 
 sys.path.append(os.getcwd())
 
@@ -9,18 +10,27 @@ from prot_graph.datasets import PDB
 from prot_graph.graphs import ResGraph
 
 
+def build_graph(pdb_db: PDB, pdb_id: str) -> ResGraph:
+
+    try:
+        struct = pdb_db.load_structure(pdb_id)
+        graph = ResGraph(struct)
+        graph.add_peptide_bonds()
+        graph.add_hydrogen_bonds()
+    except:
+        graph = None
+
+    return graph
+
+
 if __name__ == "__main__":
 
     pdb_db = PDB(pdb_dir=sys.argv[1])
-    pdb_db.load_metadata(sys.argv[2])
+    # pdb_db.load_metadata(sys.argv[2])
     figs = []
 
-    for pdb_id in tqdm.tqdm(pdb_db.filter_by_metadata("ec", "2.7.10.-")):
-        try:
-            struct = pdb_db.load_structure(pdb_id)
-            graph = ResGraph(struct)
-            graph.add_peptide_bonds()
-            graph.add_hydrogen_bonds()
-            figs.append(graph.visualize(color_node_by="type"))
-        except:
-            continue
+    graphs = process_map(functools.partial(build_graph, pdb_db), pdb_db.ids)
+    # process_map(
+    #     functools.partial(build_graph, pdb_db),
+    #     pdb_db.filter_by_metadata("ec", "2.7.10.-")
+    # )
