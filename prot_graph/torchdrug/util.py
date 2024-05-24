5fb22e52d788ec0ef765a6138828c571313ce7b3
Alexander Bock <boxkar2@gmail.com>
2024-05-24 14:32:39 -0500

    wandb logging

--- prot_graph/torchdrug/util.py
+++ prot_graph/torchdrug/util.py
@@ -134,12 +134,14 @@ def build_downstream_solver(cfg, dataset):
     solver = core.Engine(
         task, train_set, valid_set, test_set, optimizer, **cfg.engine
     )
-    engine_logger = core.WandbLogger(project="prot-graph")
-    solver.meter = core.Meter(
-        log_interval=cfg.engine.log_interval, silent=solver.rank > 0,
-        logger=engine_logger
-    )
-    solver.meter.log_config(task.config_dict())
+
+    if cfg.wandb:
+        engine_logger = core.WandbLogger(project="prot-graph")
+        solver.meter = core.Meter(
+            log_interval=cfg.engine.log_interval, silent=solver.rank > 0,
+            logger=engine_logger
+        )
+        solver.meter.log_config(task.config_dict())
 
     if "lr_ratio" in cfg:
         cfg.optimizer.params = [
