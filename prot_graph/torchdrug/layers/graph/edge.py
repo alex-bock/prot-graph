2b3843de2b6e656663e3e9fdf8a5ae1762d321b0
Alexander Bock <boxkar2@gmail.com>
2024-05-24 14:32:25 -0500

    gpu-ify

--- prot_graph/torchdrug/layers/graph/edge.py
+++ prot_graph/torchdrug/layers/graph/edge.py
@@ -32,7 +32,7 @@ class HydrogenBondEdge(nn.Module, core.Configurable):
     def forward(self, graph: data.Protein):
 
         hb_atom_ids = list(map(lambda x: graph.atom_name2id[x], HB_ATOMS))
-        is_hb_atom = torch.isin(graph.atom_name, Tensor(hb_atom_ids))
+        is_hb_atom = torch.isin(graph.atom_name, Tensor(hb_atom_ids).to(graph.device))
         hb_atom_is = is_hb_atom.nonzero().squeeze()
 
         edge_list, i = self.spatial_edge_layer(graph.subgraph(hb_atom_is))
@@ -66,7 +66,7 @@ class PeptideBondEdge(nn.Module, core.Configurable):
     def forward(self, graph: data.Protein):
 
         pep_atom_ids = list(map(lambda x: graph.atom_name2id[x], PEP_ATOMS))
-        is_pep_atom = torch.isin(graph.atom_name, Tensor(pep_atom_ids))
+        is_pep_atom = torch.isin(graph.atom_name, Tensor(pep_atom_ids).to(graph.device))
         pep_atom_is = is_pep_atom.nonzero().squeeze()
 
         edge_list, i = self.spatial_edge_layer(graph.subgraph(pep_atom_is))
@@ -101,7 +101,7 @@ class DisulfideBridgeEdge(nn.Module, core.Configurable):
     def forward(self, graph: data.Protein):
 
         db_atom_ids = list(map(lambda x: graph.atom_name2id[x], DB_ATOMS))
-        is_db_atom = torch.isin(graph.atom_name, Tensor(db_atom_ids))
+        is_db_atom = torch.isin(graph.atom_name, Tensor(db_atom_ids).to(graph.device))
         db_atom_is = is_db_atom.nonzero().squeeze()
 
         edge_list, i = self.spatial_edge_layer(graph.subgraph(db_atom_is))
