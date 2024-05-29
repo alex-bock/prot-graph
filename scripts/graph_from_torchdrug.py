03eec80952432c25b813d2df4540972f216efcc0
Alexander Bock <alex.bock@rice.edu>
2024-05-29 13:42:19 -0500

    compare graph and bn constructors

--- scripts/graph_from_torchdrug.py
+++ scripts/graph_from_torchdrug.py
@@ -27,19 +27,11 @@ if __name__ == "__main__":
     protein = pack[0]
 
     node_layer = AlphaCarbonNode()
+    edge_layers = [SpatialEdge(radius=10)]
 
-    for radius in range(5, 10):
-        edge_layers = [
-            SpatialEdge(radius=radius, min_distance=0),
-            MSTEdge(
-                base_edge_layer=SpatialEdge(radius=radius, min_distance=0),
-                p=0.0
-            ),
-            HydrogenBondEdge()
-        ]
-        bond_net_constructor = BondNetworkConstruction(
+    for construction in [GraphConstruction, BondNetworkConstruction]:
+        constructor = construction(
             node_layers=[node_layer], edge_layers=edge_layers
         )
-        bond_net = bond_net_constructor(pack)[0]
-        import pdb; pdb.set_trace()
-        ProtGraph(bond_net).visualize(hide_nodes=True)
+        network = constructor(pack)[0]
+        ProtGraph(network).visualize(hide_nodes=True)
